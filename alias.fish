89d410aeceba28f4cdac1ba61e5868c2088df96f
ChristianW-Arch <72175460+Widkidone@users.noreply.github.com>
2025-08-19 15:47:54 -0500

    Update alias.fish

--- alias.fish
+++ alias.fish
@@ -19,10 +19,65 @@ alias pksyua="paru -Syu --noconfirm"
 alias upall="paru -Syu --noconfirm"
 alias upa="paru -Syu --noconfirm"
 
-#copy shell configs
-alias cb='cp /etc/skel/.bashrc ~/.bashrc && exec bash'
-alias cz='cp /etc/skel/.zshrc ~/.zshrc && echo "Copied."'
-alias cf='cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && echo "Copied."'
+## Useful aliases
+
+# Replace ls with eza
+alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
+alias lsz 'eza -al --color=always --total-size --group-directories-first --icons' # include file size
+alias la 'eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll 'eza -l --color=always --group-directories-first --icons'  # long format
+alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+
+# Replace some more things with better alternatives
+alias cat 'bat --style header --style snip --style changes --style header'
+if not test -x /usr/bin/yay; and test -x /usr/bin/paru
+    alias yay 'paru'
+
+# Common use
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+alias ...... 'cd ../../../../..'
+alias big 'expac -H M "%m\t%n" | sort -h | nl'     # Sort installed packages according to size in MB (expac must be installed)
+alias dir 'dir --color=auto'
+alias fixpacman 'sudo rm /var/lib/pacman/db.lck'
+alias gitpkg 'pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
+alias grep 'ugrep --color=auto'
+alias egrep 'ugrep -E --color=auto'
+alias fgrep 'ugrep -F --color=auto'
+alias grubup 'sudo update-grub'
+alias hw 'hwinfo --short'                          # Hardware Info
+alias ip 'ip -color'
+alias psmem 'ps auxf | sort -nr -k 4'
+alias psmem10 'ps auxf | sort -nr -k 4 | head -10'
+alias rmpkg 'sudo pacman -Rdd'
+alias tarnow 'tar -acf '
+alias untar 'tar -zxvf '
+alias upd '/usr/bin/garuda-update'
+alias vdir 'vdir --color=auto'
+alias wget 'wget -c '
+
+# Get fastest mirrors
+alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
+alias mirrora 'sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist'
+alias mirrord 'sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist'
+alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist'
+
+# Help people new to Arch
+alias apt 'man pacman'
+alias apt-get 'man pacman'
+alias please 'sudo'
+alias tb 'nc termbin.com 9999'
+alias helpme 'echo "To print basic information about a command use tldr <command>"'
+alias pacdiff 'sudo -H DIFFPROG=meld pacdiff'
+
+#Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl 'journalctl -p 3 -xb'
 
 #shutdown or reboot
 alias ssn="sudo shutdown now"
@@ -33,8 +88,10 @@ alias cb='cp /etc/skel/.bashrc ~/.bashrc && exec bash'
 alias cz='cp /etc/skel/.zshrc ~/.zshrc && echo "Copied."'
 alias cf='cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && echo "Copied."'
 
-#Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+# Recent installed packages
+alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
 
-alias xls="eza -a --icons --color=always --group-directories-first"
-alias xll="eza -lag --icons --color=always --group-directories-first --octal-permissions"
+## Run fastfetch if session is interactive
+if status --is-interactive && type -q fastfetch
+   fastfetch --config neofetch.jsonc
+end
